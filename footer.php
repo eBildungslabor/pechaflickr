2ba85b536edd4eb65832a7f8f74e604f8d7424dc
Nele Hirsch <nele@ebildungslabor.de>
2018-11-29 16:39:04 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
-<p class="credit"><a href="http://pechaflickr.net/">pechaflickr.net</a> &bull; <a href="notes.php">notes and Q &amp; A</a> &bull; <a href="https://github.com/cogdog/pechaflickr">gitub code</a><br />pechaflickr is a cogdog production<br />
+<p class="credit">
+  <a href="http://pechaflickr.net/">Pechaflickr im englischsprachigen Original</a> &bull; <a href="notes.php">Hintergrund</a> &bull; <a href="https://github.com/cogdog/pechaflickr">gitub code</a><br />
+  Pechaflickr stammt ursprünglich von cogdog (Alan Levine) <br />
 <a href="http://cog.dog/">cog.dog</a> •  <a href="http://twitter.com/cogdog">@cogdog</a>
+  <br>Die deutsprachige Anpassung erfolgte im eBildungslabor <br />
+<a href="https://ebildungslabor.de">eBildungslabor</a> •  <a href="http://twitter.com/ebildungslabor">@ebildungslabor</a>
 </div> <!-- end wrapper -->
 
 
